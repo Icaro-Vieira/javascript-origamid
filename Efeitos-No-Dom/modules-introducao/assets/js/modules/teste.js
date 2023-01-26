ad37b748535813ac423c6e5c22707b0150341f2a
icaro vieira <icarogbov@gmail.com>
2023-01-26 16:00:32 -0300

    Separando as funções e importanto em um único arquivo (Modulando projeto)

---
+++ Efeitos-No-Dom/modules-introducao/assets/js/modules/teste.js
@@ -0,0 +1,11 @@
+export function teste1() {
+  console.log("Esse é o teste 1");
+}
+
+export function teste2() {
+  console.log("Esse é o teste 2");
+}
+
+var esseNome = "Ícaro"
+
+export const senha = 1234;
