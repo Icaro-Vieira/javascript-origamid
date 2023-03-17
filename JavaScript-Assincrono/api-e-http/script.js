e71dc4119e29b607dae889db27b4c0eec15ce15d
icaro vieira <icarogbov@gmail.com>
2023-03-17 10:47:31 -0300

    Aprendendo Sobre API e Tipos de Requisições via HTTP, resolvendo problema de CORS

---
+++ JavaScript-Assincrono/api-e-http/script.js
@@ -0,0 +1,23 @@
+// API DE POKEMON
+// fetch("https://pokeapi.co/api/v2/pokemon/1/")
+//   .then((response) => response.json())
+//   .then((pokemon) => {
+//     console.log(pokemon);
+//   });
+
+// REQUISIÇÕES E MÉTODOS DE HTTP: GET, POST, PUT, DELETE
+// const url = "https://jsonplaceholder.typicode.com/posts/";
+// const options = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json; charset=utf-8",
+//   },
+//   body: '"aula": "JavaScript"',
+// };
+
+// fetch(url, options)
+//   .then((response) => response.json())
+//   .then((json) => {
+//     console.log(json);
+//   });
+
