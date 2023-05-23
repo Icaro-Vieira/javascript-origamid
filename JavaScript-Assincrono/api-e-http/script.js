d6e2f9c2fe520a2f95d42874bd4b5d715737351b
icaro vieira <icarogbov@gmail.com>
2023-05-23 09:17:46 -0300

    Finalizando aula de API e HTTP

--- JavaScript-Assincrono/api-e-http/script.js
+++ JavaScript-Assincrono/api-e-http/script.js
@@ -1,5 +1,5 @@
 // API DE POKEMON
-// fetch("https://pokeapi.co/api/v2/pokemon/1/")
+// fetch("https://pokeapi.co/api/v2/pokemon/1")
 //   .then((response) => response.json())
 //   .then((pokemon) => {
 //     console.log(pokemon);
@@ -21,3 +21,35 @@
 //     console.log(json);
 //   });
 
+// const url = "https://jsonplaceholder.typicode.com/posts/";
+// const options = {
+//     method: "HEAD",
+// }
+
+// fetch(url, options)
+// .then(response => {
+//     console.log(response.headers.forEach(console.log))
+//     // console.log(response.headers.get("Content-Type"))
+// })
+
+
+/*
+Cross-Origin Resource Sharing, gerencia como deve ser o compartilhamento de recursos entre diferente origens.
+
+É definido no servidor se é permitido ou não o acesso dos recursos através de scripts por outros sites. Utilizando o Access-Control-Allow-Origin.
+
+Se o servidor não permitir o acesso, este será bloqueado. É possível passar por cima do bloqueio utilizando um proxy.
+
+CORS é um acordo entre browser / servidor ou servidor / servidor. Ele serve para dar certa proteção ao browser, mas não é inviolável.
+*/
+
+// Resolve o problema de CORS utilizando um Proxy
+const url = 'https://cors-anywhere.herokuapp.com/https://www.google.com/';
+const div = document.createElement('div');
+
+fetch(url)
+.then(r => r.text())
+.then(r => {
+  div.innerHTML = r;
+  console.log(div);
+});
